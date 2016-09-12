d1c7534ad4f20fe0b86765c17e8350a1f59c47c8
Rodrigo Gouveia <rodrigo.a.gouveia@gmail.com>
2016-09-12 13:27:03 -0300

    Finalizando capitula 4: adiciona karma

---
+++ cap4/js/controllers.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/* Controllers */
+
+var helloWorldControllers = angular.module('helloWorldControllers', []);
+
+
+
+helloWorldControllers.controller('MainCtrl', ['$scope', '$location', '$http',
+    function MainCtrl($scope, $location, $http) {
+        $scope.message = "Hello World";
+    }]);
+
+helloWorldControllers.controller('ShowCtrl', ['$scope', '$location', '$http',
+    function ShowCtrl($scope, $location, $http) {
+        $scope.message = "Show The World";
+    }]);
+
+helloWorldControllers.controller('CustomerCtrl', ['$scope'], 
+    function CustomerCtrl($scope)){
+    	$scope.customerName = "Bob's Burger";
+	$scope.customerNumber = "44522";
+     
+        $scope.changeCustomer = function(){
+		$scope.customerName = $scope.cName;
+		$scope.customerNumber = $scope.cNumber;
+	}; 
+    }]);
+
+helloWorldController.controller('AddCustomerCtrl', ['$scope', '$location', 
+	function AddCustomerCtrl($scope, $location){
+		$scope.submit = function(){
+			$location.path('/addCustomer/' + $scope.cName + '/' + $scope.cCity);
+		};				
+			
+	}]);
+
+helloWordController.controller('AddedCustomerCtrl', ['$scope', '$routeParams', 
+	function AddedCustomerCtrl($scope, $routeParams){
+		$scope.customerName = $routeParams.customer;
+		$scope.customerCity = $routeParams.city;
+	}
+	
+	]);
+
